5216b9f1d2c6161579d6773a9ed54c3d0706b4bb
bcyu <chanyb0827@gmail.com>
2026-04-27 18:00:38 +0900

    version: 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "kr.co.kworks.goodmorning"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
